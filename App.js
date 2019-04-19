1e506f3f5808c5854185573cc0f1b1f832a5a069
Adonai Pinheiro <adonaijpinheiro@gmail.com>
2019-04-19 18:21:32 -0300

    First Commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { Text, StyleSheet, View } from "react-native";
+
+export default class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text> App </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({});
